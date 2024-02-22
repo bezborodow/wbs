5a1ac020199c729df648019c8678bcd9f8e913f8
Damien Bezborodov <dbezborodov@gmail.com>
2024-02-22 15:49:19 +1030

    Move test data to separate file.

--- tests/test_wbs.py
+++ tests/test_wbs.py
@@ -4,14 +4,6 @@ from src.wbs import CodingScheme
 
 
 class TestWorkBreakdownStructure(unittest.TestCase):
-    def lines(self, text):
-        '''
-        Returns a list of lines from a multiline string with
-        whitespace removed.
-        '''
-        return list(map(lambda l: l.strip(), text.splitlines()))[1:-1]
-
-
     def test_scheme(self):
         scheme = CodingScheme()
         scheme.append(0, 'Aircraft System')
@@ -31,44 +23,27 @@ class TestWorkBreakdownStructure(unittest.TestCase):
         scheme.append(2, 'Antenna')
         scheme.append(2, 'Radar S/W')
 
-        expected = self.lines('''
-            1 Aircraft System
-            1.1 Air Vehicle
-            1.1.1 Air Frame
-            1.1.2 Propulsion
-            1.1.3 Application Software
-            1.2 SE Program Mgmt
-            1.3 System T&E
-            1.3.1 DT&E
-            1.3.2 OT&E
-            1.3.3 Mockups
-            2 Fire Control
-            2.1 Radar
-            2.1.1 Receiver
-            2.1.2 Transmitter
-            2.1.3 Antenna
-            2.1.4 Radar S/W
-        ''')
-        self.assertEqual(16, len(expected))
-
         actual = []
         for element in scheme:
             actual.append(f'{element.number} {element.name}')
+        self.assertEqual(16, len(actual))
 
         element = scheme.get([2, 1, 4])
         self.assertEqual('2.1.4', element.number)
         self.assertEqual('Radar S/W', element.name)
         self.assertEqual(element, scheme.get('2.1.4'))
 
-        self.assertEqual(16, len(actual))
-        for i, line in enumerate(actual):
-            self.assertEqual(expected[i], actual[i])
-            m = re.match(r'^([0-9.]+) (.*)$', line)
-            number = m.group(1)
-            name = m.group(2)
-            element = scheme.get(number)
-            self.assertEqual(number, element.number)
-            self.assertEqual(name, element.name)
+        i = 0
+        with open('tests/data/SYSTEMS', 'r') as file:
+            for line in file:
+                self.assertEqual(line.strip(), actual[i])
+                m = re.match(r'^([0-9.]+) (.*)$', line)
+                number = m.group(1)
+                name = m.group(2)
+                element = scheme.get(number)
+                self.assertEqual(number, element.number)
+                self.assertEqual(name, element.name)
+                i += 1
 
 
 if __name__ == '__main__':
